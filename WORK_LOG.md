b9517cf3a58dd9b4f6bf7aec12603b3334d2dcaf
lucas.ying@menloschool.org <lucas.ying@menloschool.org>
2024-10-30 23:10:20 -0700

    update

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,9 +1,9 @@
 Name: Lucas Ying
 
-| Date       |    Time     |                                                        Update |
-|:-----------|:-----------:|--------------------------------------------------------------:|
-| October 30 | 9:00-9:45am | Thought through process of creating my own map using buckets. |
-|            |             |                                                               |
+| Date       |    Time     |                                                                                                                                                                                                                                Update |
+|:-----------|:-----------:|--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------:|
+| October 30 | 9:00-9:45am |                                                                                                                                                                         Thought through process of creating my own map using buckets. |
+| October 30 |  8:30-11pm  | Worked through using a linked list and hashing to create my V1 solution. Currently, one of the test cases doesn't work due to some problems with spacing even though I tried ot utilize the trim function to get rid of extra spaces. |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/Finder.java
+++ src/Finder.java
@@ -1,11 +1,11 @@
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.LinkedList;
 /**
  * Finder
  * A puzzle written by Zach Blick
  * for Adventures in Algorithms
  * At Menlo School in Atherton, CA
- *
  * Completed by: [Lucas Ying]
  **/
 
@@ -16,26 +16,41 @@ public class Finder {
     public final int R = 12;
     public final int P = 100;
 
-    public Finder() {}
+    private final LinkedList<KeyValuePair>[] bucketArray;
 
-    // reads CSV file
-    public void buildTable(BufferedReader br, int keyCol, int valCol) throws IOException {
-        // TODO: Complete the buildTable() function!
-
-        String line = br.readLine();
-        for (int i =0; i < line.split("", 4).length; i++){
-
+    public Finder() {
+        // initialize the array of linked lists
+        bucketArray = new LinkedList[numberOfBuckets];
+        for (int i = 0; i < numberOfBuckets; i++) {
+            bucketArray[i] = new LinkedList<>();
         }
+    }
 
+    // reads CSV file and build the hash table
+    public void buildTable(BufferedReader br, int keyCol, int valCol) throws IOException {
+        String line;
+        while ((line = br.readLine()) != null) {
+            String[] columns = line.split(",");
+            if (columns.length > Math.max(keyCol, valCol)){
+                String key = columns[keyCol].trim();
+                String value = columns[valCol].trim();
 
+                int index = hash(key);
+                bucketArray[index].add(new KeyValuePair(key, value));
+            }
+        }
         br.close();
     }
 
     //
     public String query(String key){
         int index = hash(key);
-        LinkedList<KeyValuePair>
-
+        LinkedList<KeyValuePair> bucket = bucketArray[index];
+        for (KeyValuePair kvp: bucket){
+            if (kvp.key.equals(key)){
+                return kvp.value;
+            }
+        }
         return INVALID;
     }
 
@@ -43,9 +58,19 @@ public class Finder {
         long hashValue = 0;
         // Use a hash to compute hash value
         for (int i: key.toCharArray()) {
-            hashValue = (R + i) % P;
+            hashValue = (R * hashValue + i) % P;
         }
-        return hashValue % numberOfBuckets;
+        return (int)(hashValue % numberOfBuckets);
+    }
+
+    private static class KeyValuePair {
+        String key;
+        String value;
+
+        KeyValuePair(String key, String value){
+            this.value = value;
+            this.key = key;
+        }
     }
 
 }
